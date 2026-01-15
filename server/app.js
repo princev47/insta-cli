91e5a352af74707da565d4fa99ffac746cc00193
princev47 <most.lyprince2004@gmail.com>
2026-01-15 14:40:18 +0530

    setting

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// health route
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "InstaCLI API is running "
+  });
+});
+
+export default app;
